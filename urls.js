653be506c8e8b366e141c6b5de949959bf665d8f
Dean Hunt <dean@inkling.com>
2013-03-02 00:29:29 -0500

    First.

--- urls.js
+++ urls.js
@@ -1,9 +1,17 @@
+var Mongolian = require('mongolian');
+
 var urls = [
     { url: 'http://www.foxnews.com', cookie: '' },
     { url: 'http://news.ycombinator.com', cookie: '' },
     { url: 'http://www.theonion.com', cookie: '' },
     { url: 'http://www.slate.com', cookie: '' }
 ];
+
+exports.boot = function(req, res){
+    // Database/auth shorthand (equivalent to calling db() and auth() on the resulting server)
+    var db = new Mongolian("mongodb://public:juno-and-hera@linus.mongohq.com:10034/app12606453");
+
+};
 
 exports.list = function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -16,3 +24,6 @@ exports.add = function(req, res){
     res.write('OK');
     res.end();
 };
+
+
+// mongodb://public:juno-and-hera@linus.mongohq.com:10034/app12606453
